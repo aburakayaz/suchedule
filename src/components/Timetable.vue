0c7ffb28d032741dbb17a37ca1f3dffd591c12ba
Alper Berber <berber@sabanciuniv.edu>
2020-06-15 15:23:02 +0300

    Created timetable

---
+++ src/components/Timetable.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="timetable">
+
+        <table class="timetable">
+            <thead>
+                <tr>
+                    <th class="hour-cell"></th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="hour-cell">8.40</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="hour-cell">9.40</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'Timetable'
+}
+</script>
+<style scoped src="@/assets/css/timetable.css">
+    
+</style>
