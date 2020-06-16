7ca82477f0d4f7f4eacf5522fac6c8cbe213efff
Alper Berber <berber@sabanciuniv.edu>
2020-06-16 19:26:40 +0300

    Updated Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -31,31 +31,32 @@
         <div class="row courses mt-3">
             <div class="col-md-12">
 
-                <div v-for="course in this.$store.state.schedule.allCourses" :key="course.code" :id="`course_${course.code.trim()}`"  class="course hide-sections">
-                    <div class="header" @click="toggleSections(course.code.trim())">
+                <div v-for="course in this.$store.state.schedule.allCourses" data-code="AL102" :key="course.code" :id="`course_${shortenCode(course.code)}`"  class="course hide-sections">
+                    <div class="header" :data-code="shortenCode(course.code)" @click="toggleSections(shortenCode(course.code), $event)">
                         <div class="title">
                         <strong>{{ course.code }}</strong> - {{ course.name }}
                     </div>
                     <div class="caret">
-                        <i class="fal fa-angle-down"></i>
+                        <i class="fal fa-angle-right"></i>
                     </div>
                     </div>
                     <div class="sections">
                         <div class="section-header">Lectures</div>
-                        <div v-for="section in course.classes[0].sections" :key="section.crn" class="section-container">
+                        <div v-for="section in course.classes[0].sections" :key="section.crn" class="section-container" @mouseenter="highlightTable(section)">
                             <div  class="row">
                                 <div class="col-md-9 section-info">
                                     <span class="instructor-title">Instructors</span>
                                     <strong>{{ instructorName(section.instructors) }}</strong>
                                     <div class="section-days">
-                                        <div v-for="schedule in section.schedule" :key="schedule.day+schedule.duration+schedule.start+schedule.day" class="section-day">
+                                        <div v-for="(schedule, index) in section.schedule" :key="index"  class="section-day">
                                             <span v-if="schedule.day == 0">Mon </span>
                                             <span v-else-if="schedule.day == 1">Tue </span>
                                             <span v-else-if="schedule.day == 2">Wed </span>
                                             <span v-else-if="schedule.day == 3">Thu </span>
                                             <span v-else-if="schedule.day == 4">Fri </span>
-
+                                            &nbsp;
                                             <span>{{ schedule.start+8 }}.40 - {{ schedule.start+8+schedule.duration }}.40 </span>
+                                            &nbsp;
                                             {{ getPlace(schedule.place) }}
                                         </div>
                                     </div>
@@ -64,7 +65,7 @@
                                             {{ section.group }}
                                         </span>
                                         <span class="go-info">
-                                            <router-link to="#" class="info-link">info</router-link>
+                                            <router-link to="#" class="info-link">info <i class="fal fa-external-link"></i></router-link>
                                         </span>
                                     </span>
                                 </div>
@@ -85,6 +86,7 @@
 </template>
 <script>
 import store from '../_store/store';
+import $ from 'jquery';
 export default {
     name: 'Sidebar',
     data() {
@@ -102,21 +104,29 @@ export default {
     computed: {
         
     },
+    mounted(){
+    },
     methods: {
         toggleDay(day){
             this.days[day] = !this.days[day];
         },
-        toggleSections(code){
-            var element = document.getElementById(`course_${code}`)
-            if(element.classList.contains('hide-sections')){
-                element.classList.remove('hide-sections')
-                element.siblings(':not(hide-sections)').addClass('hide-sections');
-                
+        highlightTable(section){
+            for(let i = 0; i < section.schedule.length; i++){
+                store.commit('highlightTable', section);
+            }
+        },
+        toggleSections(code, event){
+            console.log(event);
+            var element = $(`#course_${code}`)
+            if(element.hasClass('hide-sections')){
+                $(element).siblings(':not(hide-sections)').addClass('hide-sections');
+                element.removeClass('hide-sections')
+                $(`#course_${code} .caret i`).removeClass("fa-angle-right").addClass("fa-angle-down")
             }
             else{
-                element.classList.add('hide-sections')
                 element.siblings(':not(hide-sections)').addClass('hide-sections');
-                
+                element.addClass('hide-sections') 
+                $(`#course_${code} .caret i`).addClass("fa-angle-right").removeClass("fa-angle-down")   
             }
         },
         toggleSidebar(){
@@ -140,6 +150,9 @@ export default {
         },
         getPlace(id){
             return this.$store.getters.getAllPlaces[id];
+        },
+        shortenCode(code){
+            return code.split(" ")[0]+code.split(" ")[1]
         }
     },
 }
