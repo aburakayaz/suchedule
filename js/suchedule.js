16f9bb5b14ce81cca698ec1f5e4146872177b351
burak <burak@voys.ai>
2021-02-08 03:52:51 +0300

    Fix and update data for term 202002

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,7 +1,7 @@
 const config = Object.freeze({
-    term: '202001',
+    term: '202002',
     infoLink: 'https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=202001&crn_in=',
-    dataVersion: 16
+    dataVersion: 17
 });
 
 const templateGenerator = (() => {
