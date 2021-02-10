93b50bc267582a17b9a749849ec6dd8cf60e8b09
Burak Ayaz <adnan.burak.ayaz@gmail.com>
2021-02-10 18:18:46 +0300

    Change term link

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,6 +1,6 @@
 const config = Object.freeze({
     term: '202002',
-    infoLink: 'https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=202001&crn_in=',
+    infoLink: 'https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=202002&crn_in=',
     dataVersion: 17
 });
 
