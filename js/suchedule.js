15b91a85ef38e4025c9e6341e23fa9810d1901b5
burak <burak@voys.ai>
2021-10-01 23:51:06 +0300

    Update data

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,7 +1,7 @@
 const config = Object.freeze({
     term: '202101',
     infoLink: 'https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=202101&crn_in=',
-    dataVersion: 22
+    dataVersion: 23
 });
 
 const templateGenerator = (() => {
