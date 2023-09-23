b95c0e84c1980d30cee4d4d807047dc0aca3e681
Furkan K. Yıldırım <furkankyildirim@gmail.com>
2023-09-23 16:58:04 +0300

    Migrate to data v35

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,6 +1,6 @@
 const config = {
     term: '202301',
-    dataVersion: 34
+    dataVersion: 35
 };
 
 config.infoLink = `https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=${config.term}&crn_in=`;
