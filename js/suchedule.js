1df367dc6f3e355fd1b992b6c3ee1b0f50a2ca79
furkankyildirim <furkan.k.yildirim@gmail.com>
2024-08-08 15:56:26 +0300

    Updates data 202401

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,6 +1,6 @@
 const config = {
-    term: '202302',
-    dataVersion: 38
+    term: '202401',
+    dataVersion: 39
 };
 
 config.infoLink = `https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=${config.term}&crn_in=`;
