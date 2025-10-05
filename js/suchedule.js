13d6fcb31a3eae04c4721f1b1553d43ebc9f5e1b
aburakayaz <aburakayaz@users.noreply.github.com>
2025-10-05 00:28:08 +0000

    chore: update data to v66

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,6 +1,6 @@
 const config = {
     term: '202501',
-    dataVersion: 65
+    dataVersion: 66
 };
 
 config.infoLink = `https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=${config.term}&crn_in=`;
